d26bd1a4c391d8b5cbd99d16a744be125b914c8e
Anshik01 <anshik0706@gmail.com>
2025-02-21 16:58:29 +0530

    Project that takes custom name from api

--- src/main/java/com/uc/hello/hellocontroller/HelloController.java
+++ src/main/java/com/uc/hello/hellocontroller/HelloController.java
@@ -1,6 +1,8 @@
 package com.uc.hello.hellocontroller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +12,11 @@ public class HelloController {
     @RequestMapping(value = {"", "/"})
     public String sayHello(){
         return "Hello from BridgeLabz.";
+    }
+
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHelloWithName(@RequestParam(value = "name") String name){
+        return "Hello " + name + "!";
     }
 
 }
